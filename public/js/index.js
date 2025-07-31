5af8ba66e9823519567e5235069211148f0d1e80
HOSEKI7 <farzan.clash2004@gmail.com>
2025-07-31 23:19:27 +0700

    carousel-change-bg

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,18 @@
 $(document).ready(function () {
   $(".carousel").carousel();
 });
+
+function changeBg(bg, title) {
+  const banner = document.querySelector(".banner");
+  const contents = document.querySelector(".content");
+  banner.style.background = `url("/img/${bg}")`;
+  banner.style.backgroundSize = "cover";
+  banner.style.backgroundPosition = "center";
+
+  contents.array.forEach((element) => {
+    contents.classList.remove("active");
+    if (contents.classList.contains(title)) {
+      contents.classList.add("active");
+    }
+  });
+}
